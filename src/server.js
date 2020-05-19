18d71b2c13fbd699aff60848c17b51b4fc01ae2c
Himanshu Kumar <gopihimanshu@gmail.com>
2020-05-19 18:32:07 +0530

    Added few files

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const index = require('./routes/index');
 
 const ROOT_PATH = process.cwd();
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -10,6 +11,8 @@ app.use(express.static(publicDirectoryPath));
 
 app.set('views', path.join(ROOT_PATH, '/views/templates'));
 app.set('view engine', 'pug');
+
+app.use('/', index);
 
 app.get('/', function (req, res) {
     res.render('index', { title: 'Hey', message: 'Hello there!' })
--- views/index.js
+++ views/index.js
@@ -5,7 +5,8 @@ export default class Root extends React.Component{
     render(){
         return (
             <div>
-              <p>Hello World</p>
+              <p>Hello World !!</p>
+              <footer>{CREATED_BY}</footer>
             </div>
         );
     }
